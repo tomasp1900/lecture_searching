0bf50b444acf159170afb58c0c1cc54ff12e3dce
Tomas Prchlik <tomasp1900@gmail.com>
2025-04-07 13:04:33 +0200

    mesasge

--- searching.py
+++ searching.py
@@ -11,10 +11,22 @@ def read_data(file_name, field):
     :param field: (str), field of a dict to return
     :return: (list, string),
     """
+
+    if field not in {"unordered_numbers","ordered_numbers","dna_sequence"}:
+        return None
     file_path = os.path.join(cwd_path, file_name)
 
+    with open(file_path, "r") as json_file:
+        seq = json.load(json_file)
+
+    return seq[field]
+
+def linear_search(prohledavana_sekvence, hledane_cislo):
 
 def main():
+    file_name = ("sequential.json")
+
+    seq = read_data(file_name, field="")
     pass
 
 
